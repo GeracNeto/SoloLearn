6b4ab9727dd6333cd218a685ea3772d6699337ce
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-25 15:37:14 -0300

    Resolvido desafio 'Loading' https://www.sololearn.com/learning/1024?modal=unlock&ccid=7617

---
+++ Javascript/24-2_Loading.js
@@ -0,0 +1,3 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=761
+var showPreloader = function () { return console.log('Loading...'); };
+showPreloader();
---
+++ Javascript/24-2_Loading.ts
@@ -0,0 +1,5 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=761
+
+const showPreloader = () => console.log('Loading...')
+
+showPreloader()
