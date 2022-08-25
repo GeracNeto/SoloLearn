0b3f64ec202261a1b36a4f40c1a28996d668b6a0
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-25 15:14:43 -0300

    Resolvido desafio 'Office computers' https://www.sololearn.com/learning/1024?modal=unlock&ccid=392

---
+++ Javascript/7-2_Office_Computers.js
@@ -0,0 +1,4 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=1233
+var computers = 0;
+var monitorsQty = function (numOfComputers) { return numOfComputers * 2; };
+console.log(monitorsQty(10));
---
+++ Javascript/7-2_Office_Computers.ts
@@ -0,0 +1,7 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=1233
+
+const computers = 0
+
+const monitorsQty = (numOfComputers: number) => numOfComputers * 2
+
+console.log(monitorsQty(10))
