b2ad2b5a4a69004c576e40a2a6790776443abf8e
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-25 15:15:22 -0300

    Resolvido desafio 'Find The Adults' https://www.sololearn.com/learning/1024?modal=unlock&ccid=392

---
+++ Javascript/9-2_Find_The_Adults.js
@@ -0,0 +1,3 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=392
+var isAdult = function (age) { return age >= 18 ? true : false; };
+console.log(isAdult(17));
---
+++ Javascript/9-2_Find_The_Adults.ts
@@ -0,0 +1,5 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=392
+
+const isAdult = (age: number) => age >= 18 ? true : false
+
+console.log(isAdult(17))
