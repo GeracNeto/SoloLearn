a6c0fa2cf490e4a9b34437cdd51e3fac18b91421
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-25 15:25:41 -0300

    Resolvido desafio 'Vacation Month' https://www.sololearn.com/learning/1024?modal=unlock&ccid=1235

---
+++ Javascript/14-2_Vacation_Month.js
@@ -0,0 +1,32 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=1235
+var isVacation = function (month) {
+    switch (month) {
+        case 'January':
+            break;
+        case 'February':
+            break;
+        case 'March':
+            break;
+        case 'April':
+            break;
+        case 'May':
+            break;
+        case 'June':
+            break;
+        case 'July':
+            break;
+        case 'August':
+            return 'vacation';
+        case 'September':
+            break;
+        case 'October':
+            break;
+        case 'November':
+            break;
+        case 'December':
+            break;
+        default:
+            break;
+    }
+};
+console.log(isVacation('August'));
---
+++ Javascript/14-2_Vacation_Month.ts
@@ -0,0 +1,35 @@
+// https://www.sololearn.com/learning/1024?modal=unlock&ccid=1235
+
+const isVacation = (month: string) => {
+
+    switch (month) {
+        case 'January':
+            return '';
+        case 'February':
+            return '';
+        case 'March':
+            return '';
+        case 'April':
+            return '';
+        case 'May':
+            return '';
+        case 'June':
+            return '';
+        case 'July':
+            return '';
+        case 'August':
+            return 'vacation';
+        case 'September':
+            return '';
+        case 'October':
+            return '';
+        case 'November':
+            return '';
+        case 'December':
+            return ''
+        default:
+            return '';
+    }
+}
+
+console.log(isVacation('August'))
